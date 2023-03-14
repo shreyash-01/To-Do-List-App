6e6c240fc3f5bcf7eb8f6f53ff6186f834ae9756
shreyash-01 <singhshreyash0075@gmail.com>
2023-03-14 15:34:32 +0530

    added toolbar to the app

--- app/src/main/java/com/shreyash/todo/MainActivity.java
+++ app/src/main/java/com/shreyash/todo/MainActivity.java
@@ -1,14 +1,20 @@
 package com.shreyash.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
